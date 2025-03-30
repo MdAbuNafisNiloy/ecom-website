4af21beef02df055f7381e6a93deb2cc8911318c
MdAbuNafisNiloy <162716351+MdAbuNafisNiloy@users.noreply.github.com>
2025-03-30 17:35:38 +0000

    updated pocketbase.js

--- src/pocketbase.js
+++ src/pocketbase.js
@@ -1,8 +1,8 @@
 import PocketBase from "pocketbase";
 
 // const pb = new PocketBase("http://127.0.0.1:8090"); 
-// const pb = new PocketBase("https://pocket.alphasoft.world"); 
-const pb = new PocketBase("https://api.friendsonlineshop.com");
+const pb = new PocketBase("https://pocket.alphasoft.world"); 
+// const pb = new PocketBase("https://api.friendsonlineshop.com");
 pb.autoCancellation(false);
 
 export default pb;
